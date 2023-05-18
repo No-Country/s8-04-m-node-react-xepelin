20c345557f07900d1de41f29334ce106300c3015
Isaac Urdaneta <urdanetacuarteisaacdavid@gmail.com>
2023-05-18 19:06:46 -0400

    principal icon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,9 +18,9 @@ function App() {
         <Route path="/profile" element={<UserInfo />} />
         
       </Routes>
-      
+      <Footer/>
     </Router>
-    <Footer/>
+    
     </div>
   );
 }
--- frontend/src/assets/images.js
+++ frontend/src/assets/images.js
@@ -1,2 +1,11 @@
 //este archivos es para importar las imagenes y exportarlas todas en un unico objeto.
 
+
+//icono principal.
+import zepelin from "./icono principal/zepelin.png";
+
+
+export {
+
+    zepelin,
+}
