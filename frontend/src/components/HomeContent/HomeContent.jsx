1c3f64141cede72e275cc5b1e3363c843b474126
Isaac Urdaneta <urdanetacuarteisaacdavid@gmail.com>
2023-05-20 19:29:37 -0400

    working at home yet

--- frontend/src/components/HomeContent/HomeContent.jsx
+++ frontend/src/components/HomeContent/HomeContent.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { AiOutlineArrowRight } from "react-icons/ai";
 import videoPrincipal from "../../assets/video/dashboard-xepelin.mp4";
-import { cmp, wom } from '../../assets/images';
-
+import { cmp, flesan, izquierdo, mega, sacyr, wom } from '../../assets/images';
+import Button from "../button/Button.jsx";
 
 const HomeContent = () => {
   return (
@@ -16,7 +17,7 @@ const HomeContent = () => {
                     
                 </h1>
                 <p className='mt-[1.5rem] text-[1.125rem] leading-7'>
-                     <span className='font-bold'> Paga</span> tus cuentas, <span className='font-bold'> adelanta</span>  el pago de tus facturas y soluciona tus problemas de liquidez - en una sola plataforma <span className='font-bold'> 100% digital.</span> 
+                    <span className='font-bold'> Paga</span> tus cuentas, <span className='font-bold'> adelanta</span>  el pago de tus facturas y soluciona tus problemas de liquidez - en una sola plataforma <span className='font-bold'> 100% digital.</span> 
                 </p>
                 <div className='grid gap-3 grid-cols-1 md:grid-cols-2 pt-6'>
                     <div className='col-span-1'>
@@ -38,18 +39,49 @@ const HomeContent = () => {
                 Más de 20,000 empresas confían en el financiamiento de Xepelin
             </h2>
 
-            <div className="grid grid-cols-16 gap-5 md:gap-12">
-                <div className=" col-span-4 md:col-span-3 flex items-center justify-center">
+            
+
+            <div className="my-8 py-8 max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-6 gap-6">
+                <div className="col-span-1 flex items-center justify-center">
                     <img src={wom} alt='wom' />
                 </div>
-
-                <div className=" col-span-4 md:col-span-2 flex items-center justify-center">
-                    <img src={cmp} alt='cmp' />
+                <div className="col-span-1 flex items-center justify-center">
+                <img src={cmp} alt='cmp' />
+                </div>
+                <div className="col-span-1 flex items-center justify-center">
+                <img src={flesan} alt='flesan'/>
+                </div>
+                <div className="col-span-1 flex items-center justify-center">
+                <img src={sacyr} alt='sacyr' />
+                </div>
+                <div className="col-span-1 flex items-center justify-center">
+                <img src={izquierdo} alt='izquierdo' />
+                </div>
+                <div className="col-span-1 flex items-center justify-center">
+                <img src={mega} alt='mega' />
                 </div>
             </div>
+            
+            <div className='w-full px-10 mx-auto'>
+                <div className='flex flex-col md:flex-row justify-center items-center bg-white py-6 px-2 rounded-xl'>
+                    <h2 className='my-2 mx-2'>
+                    Explora nuestro producto:
+                    </h2>
+                    <p className='my-2 mx-2 font-bold'>Payments</p>
+
+                    <Button>
+                        <div className='flex items-center justify-center'>
+                        <span className='mx-2'>Conoce mas.</span>
+                        <AiOutlineArrowRight/>
+                        </div>
+                    </Button>   
+                </div>
+            </div>
+
         </div>
     </main>
-  )
+    )
 }
+
 
 export default HomeContent;
