4956a62e593b7a322755e5a26f7bd02be1dd2cb2
fredy alberto varon guzman <fvaron@itfip.edu.co>
2023-05-22 16:25:26 -0500

    add page mision (#16)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import UserInfo from './pages/UserInfo';
 import NavBar from './components/Navbar/NavBar';
 import Footer from './components/Footer/Footer';
 import Login from './pages/Login';
+import Mision from './pages/Mision';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<UserInfo />} />
         <Route path="/login" element={<Login/>} />
+        <Route path='/mision' element={<Mision/>}/>
       </Routes>
       <Footer/>
     </Router>
